fac1055e14cb46d7cd1103a9a3ba7b5c062288cf
Anukesh07 <53857067+Anukesh07@users.noreply.github.com>
2019-09-24 00:40:09 +0530

    Create linklist.c

---
+++ linklist.c
@@ -0,0 +1,51 @@
+# include<stdio.h>
+struct list 
+{
+	int data;
+	struct*next ;
+	typedef struct*list NODE ;
+	NODE create() 
+	{
+		NODE p=(NODE)malloc(struct list)
+		if(p==null)
+		{
+		 printf("memory not allocated") ;
+		 return NULL ;
+		}
+		return p ;
+	}
+	NODE insertfront(int ele,NODE head)
+	NODE p=create() ;
+	p ->data=item ;
+	p ->next=head ;
+	head=p
+	return head ;
+}
+void display
+{ 	
+	NODE p ;
+	if(head==NULL)
+	{ 
+	 printf("list is empty \n") ;
+	 exit(0) ;
+	}	
+	P=head
+	while(p=NULL)
+	{
+	 printf("%d\n",p->data) ;
+	 p=p->next ;
+	}
+}
+void inssert end(int item,NODE head)
+{
+	NODE p,q ;
+	q=create() ;
+	q-> data=item ;
+	p=head ;
+	while(p->next!=NULL)
+	{
+	 p=p->next ;
+	}
+	p->next=q ;
+	q->next=NULL ;
+}
